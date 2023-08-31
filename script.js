2d9a358fc0a76b8483add22f6e2f6485bb609523
Nicolas Falciglio <falciglionicolas@gmail.com>
2023-08-31 10:14:56 -0300

    modificaciones

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+const mostrarError = (mensaje) => alert(mensaje);
+
+const calcularPago = (precioVehiculo, cantidadCuotas) => Math.round(precioVehiculo / 1500 * cantidadCuotas);
+
 function ingresarMonto() {
     while (true) {
         // Pide al usuario que ingrese una opción
-        let opcion = parseInt(prompt("Elegir opción que desea realizar: \n 1-contra todo riesgo \n 2-completo \n 3-contra terceros"));
+        let opcion = parseInt(prompt("Elegir opción: \n 1-contra todo riesgo \n 2-completo \n 3-contra terceros"));
 
         // Ingresar el nombre del vehículo y el precio
-        let nombreVehiculo = prompt("Ingrese el nombre del vehículo:");
-        let precioVehiculo = parseFloat(prompt("Ingrese el precio del vehículo:"));
+        let nombreVehiculo = prompt("Ingrese marca y modelo del vehículo:");
+        let precioVehiculo = Number(prompt("Ingrese el precio aproximado del vehículo:"));
 
         // Comprueba que el precio sea un valor numérico
         if (isNaN(precioVehiculo)) {
             mostrarError("Por favor, ingrese un valor numérico válido para el precio.");
-            return;
+            continue;
         }
 
         // calcular la cuota
@@ -22,12 +26,10 @@ function ingresarMonto() {
         }
 
         // Cálculo de la cuota del seguro
-        let pagoPorCuota = Math.round(precioVehiculo / 1500 * cantidadCuotas);
+        let pagoPorCuota = Number(precioVehiculo / 1500 * cantidadCuotas);
 
-        alert(`El pago por cuota para el vehículo ${nombreVehiculo} es: ${pagoPorCuota}`);
+        alert("El pago por cuota para el vehículo " + nombreVehiculo + " es: " + pagoPorCuota);
     }
 }
 
-const mostrarError = (mensaje) => alert(mensaje);
 
-const calcularPago = (precioVehiculo, cantidadCuotas) => Math.round(precioVehiculo / 1500 * cantidadCuotas);
